06b89bd33bdd65cca3e336d51384e7aa12effde6
do van phong <dodaiphong299@gmail.com>
2024-11-27 23:34:24 +0700

    create CORS

--- backend/applicationWebApi/Program.cs
+++ backend/applicationWebApi/Program.cs
@@ -1,13 +1,25 @@
-﻿using BookStore.DAL.UnitOfWorks;
-using BookStore.DAL.Repositories;
+﻿
 using BookStore.BLL.Services;
-using BookStore.DataAccess.DataContext;
+using Microsoft.AspNetCore.Cors;
 using BookStore.DAL;
 using BookStore.DataAccess;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")  // Cho phép origin này
+              .AllowAnyHeader()                    // Cho phép bất kỳ header nào
+              .AllowAnyMethod()                    // Cho phép tất cả các HTTP methods (GET, POST, PUT, DELETE,...)
+              .AllowCredentials();                 // Cho phép sử dụng credentials (cookies, HTTP authentication,...)
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -30,6 +42,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Use CORS policy
+app.UseCors("AllowLocalhost");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
